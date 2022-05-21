4eaf00868e1449326351cad102f58dfe89d36bd8
GiaHy123 <honggiahy@gmail.com>
2022-05-21 22:24:48 +0700

    Done Scale

--- HealthTracking/src/Screens/Scale.js
+++ HealthTracking/src/Screens/Scale.js
@@ -1,179 +1,141 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
-  Image
+  Image,
+  Modal
 } from 'react-native';
 import CustomButton from '../Utils/CustomButton';
 import { FirebaseManager } from '../Utils/FirebaseManager';
-import ModalAlert from '../Utils/ModelAlert';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Scale = () => {
-  const [bmi, setBmi] = useState('');
+  const [bmi, setBmi] = useState(null);
   const [info, setInfo] = useState('');
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState();
   const [getWater, SetgetWater] = useState();
   const [currentDate, setCurrentDate] = useState('');
-  const [InputAccess,SetInputAccess] = useState(false);
-  const [InputAttempt,setInputAttemp] = useState(false);
-  const [alertNoinput,setAlertNoinput] =useState(false)
+  const [alertNoinput, setAlertNoinput] = useState(false);
   const manager = new FirebaseManager();
-  const [saveToday,setSaveToday] = useState(new Date());
-  const [isUpdating,setIsUpdating] = useState(false);
-  
-  const [data,setData] = useState(manager.dataHealthInfor);
 
-    const uploadData = async()=> {
-      
-      var date = new Date(Date.now());
-        data.day = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();        
-        //data.Height= height;
-        //data.Weight= weight;
-        //handleBmi();
-        //data.BMI = bmi;     
-      var username = manager.GetUserName();
-        data.userName=username;
-        manager.AddDataRandomDoc("HealthInfo", data);
-        
-        console.log("Done upload Data")
+  const [data, setData] = useState(manager.dataHealthInfor);
+
+  const HandleChange = async () => {
+    if (height == null || weight == null) {
+      ModalAlertNoInputOn();
+    } else {
+    var date = new Date(Date.now());
+    data.day = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    data.Height = height;
+    data.Weight = weight;
+    await handleBmi();
+    const query = ["day", '==', data.day.toString()]
+    var arr = await manager.getDataWithQuery("HealthInfo", "day", "==", data.day);
+      if (arr.length == 0) {
+        manager.AddDataRandomDoc("HealthInfo", data)
+      }
+      else {
+        await manager.UpdateData("HealthInfo", data, query)
+      } 
     }
-    
+  }
 
-    const HandleChange =async()=>{
-      setInputAttemp(false);
-      setIsUpdating(true);
-      var date = new Date(Date.now());
-        data.day = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();        
-        data.Height= height;
-        data.Weight=weight;
-        handleBmi();
-        data.BMI = bmi;
-      var username = manager.GetUserName();
-        data.userName=username;
-        
-        const query = ["day",'==',data.day.toString()]
-        await manager.UpdateData("HealthInfo",data,query)
-      
-      console.log('Update Data thanh cong')
-    }
-
-  useEffect(()=>{
-    getDataSaveToday();
-    var thisDay =new Date(Date.now());
+  useEffect(() => {
+    var thisDay = new Date(Date.now());
     var date = new Date().getDate()
     var month = new Date().getMonth() + 1
     var year = new Date().getFullYear()
     var hour = new Date().getHours()
     var minutes = new Date().getMinutes()
     setCurrentDate(
-      date +'/' + month +'/'+ year + ' - '+ hour +':' + minutes
+      date + '/' + month + '/' + year + ' - ' + hour + ':' + minutes
     )
-        data.day = thisDay.getDate() + '/' + (thisDay.getMonth()+1) + '/' + thisDay.getFullYear();
-        const getThisDay = manager.getDataWithCollection("HealthInfo","day","==",data.day.toString())
-        if(getThisDay == data.day){
-          setInputAttemp(true);
-        }else setInputAttemp(false);
-  },[]);
+    data.day = thisDay.getDate() + '/' + (thisDay.getMonth() + 1) + '/' + thisDay.getFullYear();
+  }, []);
 
-  const ModalAlertOn=() =>{
-    SetInputAccess(true);
-  }
-  const ModalAlertNoInput=() =>{
+  const ModalAlertNoInputOn = () => {
     setAlertNoinput(true);
   }
-
-  
-   
-const getDataSaveToday = async () => {
-  try {
-    const jsonValue = await AsyncStorage.getItem('saveDate')
-    console.log('getDataDateOK');
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-
-  } catch(e) {
-    console.log('getDataSaveToday error!');
-  }
-}
-
-  const saveDate = async (value) => {
-    try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem('saveDate', jsonValue)
-      console.log('save day ok');
-    } catch (e) {
-      console.log('error saveDate');
-    }
+  const ModalAlertNoInputOff = () => {
+    setAlertNoinput(false);
   }
 
-  const handleEnter =() =>{
-    if(InputAttempt == true){
-      ModalAlertOn(true);
+  const handleEnter = async () => {
+    if (height == null || weight == null) {
+      ModalAlertNoInputOn();
+    } else {
+      handleBmi();
     }
-    if(InputAttempt == false){
-      if(height== null|| weight == null){
-        ModalAlertNoInput(true);
-    }else {handleBmi();
-      //so lan nhap
-    if(isUpdating == false){
-      setInputAttemp(true);
-      //up data len database
-      uploadData();
-    }
-    }
-    }
-    
   }
-
 
   const handleBmi = () => {
     let val = (
       [Number(weight) / Number(height) / Number(height)] * 10000
     ).toFixed(1);
     setBmi(val);
+    data.BMI = val;
     let waterInput = Number(weight) * 0.03;
     SetgetWater(waterInput);
-  
+
     if (val < 18.5) {
-      setInfo('Under Weight');
+      setInfo('Gầy');
     } else if (val > 18.5 && val <= 24.9) {
-      setInfo('Healthy');
+      setInfo('Khỏe mạnh');
     } else if (val > 24.9 && val < 30) {
-      setInfo('Overweight');
+      setInfo('Mũm mỉm');
     } else {
-      setInfo('Obese');
+      setInfo('Béo');
     }
-    //luu ngay nhap
-    let today = new Date(Date.now());
-    setSaveToday(today);
-    console.log(saveToday);
-    saveDate(saveToday);
   };
 
   return (
     <View style={styles.body}>
-      <View style ={{marginTop:'5%',marginRight:'45%'}}>
+      <View style={{ marginTop: '5%', marginRight: '45%' }}>
         <Text style={styles.time}>{currentDate}</Text>
       </View>
       <View>
-        <Text style={{fontSize:35,fontFamily: 'PlayBall-Regular',color:'#EF5DA8'}}>Scale Input</Text>
+        <Text style={{ fontSize: 35, fontFamily: 'PlayBall-Regular', color: '#EF5DA8' }}>Phân tích chỉ số</Text>
       </View>
-      <ModalAlert
-            visible={InputAccess}
-            close={()=>SetInputAccess(false)}
-            content='Bạn chỉ được nhập 1 lần trong ngày!'
-          />
-      <ModalAlert
-            visible={alertNoinput}
-            close={()=>setAlertNoinput(false)}
-            content='Cần nhập chỉ số!'
-          />
+      <View style={{ justifyContent: 'center' }}>
+        <Modal
+          visible={alertNoinput}
+          transparent
+          onRequestClose={alertNoinput}
+          animationType='fade'>
+          <View style={styles.container}>
+            <View style={styles.view}>
+              <View style={styles.header}>
+
+                <Text style={styles.text1}>Cảnh báo</Text>
+              </View>
+              <View style={styles.content}>
+                <Text style={{ fontSize: 22 }}>
+                  Hãy nhập chỉ số vào trước!
+                </Text>
+                <Image
+                  style={styles.ImageStyle}
+                  source={require('../Image/warning.png')}
+                />
+              </View>
+              <View style={{ marginLeft: '80%' }}>
+                <TouchableOpacity onPress={ModalAlertNoInputOff}>
+                  <Image
+                    source={require('../Image/OK.png')}
+                    style={{ height: 30, width: 30 }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
+
       <View style={styles.InformationBox}>
-        <Text style={styles.text}>Height : </Text>
+        <Text style={styles.text}>Chiều cao: </Text>
         <TextInput
           style={styles.textInput}
           onChangeText={value => setHeight(value)}
@@ -183,7 +145,7 @@ const getDataSaveToday = async () => {
       </View>
 
       <View style={styles.InformationBox}>
-        <Text style={styles.text}>Weight : </Text>
+        <Text style={styles.text}>Cân nặng : </Text>
         <TextInput
           style={styles.textInput}
           onChangeText={value => setWeight(value)}
@@ -192,51 +154,38 @@ const getDataSaveToday = async () => {
         />
       </View>
       <View>
-      <CustomButton
-        content = "Enter"
-        color = "#EF5DA8"
-        size = {25}
-        style = {styles.enter}
-        onPress = {handleEnter}
-        width ={150}
-        height = {50}
-      />
-      
-        <TouchableOpacity onPress={HandleChange}>       
+        <CustomButton
+          content="Tính"
+          color="#EF5DA8"
+          size={25}
+          style={styles.enter}
+          onPress={handleEnter}
+          width={150}
+          height={50}
+        />
+
+        <TouchableOpacity onPress={HandleChange}>
           <Image
-                source={require('../Image/Change.png')}
-                style={{marginLeft:'38%',marginTop:'-15%'}}
+            source={require('../Image/Change.png')}
+            style={{ marginLeft: '38%', marginTop: '-15%', shadowColor: 'black', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.8, shadowRadius: 2 }}
           />
-        
+
         </TouchableOpacity>
       </View>
-      {/* <Pressable
-        onPress={handleBmi}
-        style={({pressed}) => [
-          {backgroundColor: pressed ? '#FCD0D0' : '#EF5DA8'},
-          styles.enter,
-        ]}>
-        <Text style={{fontSize: 20, color: '#000'}}>Enter</Text>
-      </Pressable> */}
 
-      <View style={styles.InformationBox}>
-        <View>
-          <Text style={styles.text}>
-            BMI: {bmi}   {info}
-          </Text>
-        </View>
-
+      {(bmi == null) ? null : (
         <View style={styles.InformationBox}>
-          <Text style={styles.text}>Water need: {getWater} </Text>
+          <View>
+            <Text style={styles.text}>
+              BMI: {bmi}   {info}
+            </Text>
+          </View>
+
+          <View style={styles.InformationBox}>
+            <Text style={styles.text}>Lượng nước cần: {getWater} lít</Text>
+          </View>
         </View>
-      </View>
-      
-      <View style ={{marginTop: 17,marginLeft:105}}>
-        <Text style ={styles.noteScale}>
-          liter
-        </Text>        
-      </View>
-      
+      )}
     </View>
   );
 };
@@ -252,8 +201,14 @@ const styles = StyleSheet.create({
     color: '#000000',
     fontSize: 25,
     fontStyle: 'italic',
-    
+
     margin: 20,
+  },
+  text1: {
+    color: '#000000',
+    fontSize: 25,
+    fontStyle: 'italic',
+    margin: 10
   },
   InformationBox: {
     backgroundColor: '#FCD0D0',
@@ -272,30 +227,60 @@ const styles = StyleSheet.create({
   textInput: {
     fontSize: 25,
     color: '#000000',
-    marginLeft: "35%",
+    marginLeft: "40%",
     marginTop: '-16%'
   },
   enter: {
     marginTop: '5%',
     width: 110,
     height: 45,
-    //backgroundColor: '#FAA1A1',
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: '50%',
     borderRadius: 20,
   },
-  noteScale:{
+  noteScale: {
     color: '#000000',
     fontSize: 25,
     fontStyle: 'italic',
-    margin:'7.5%'
+    margin: '7.5%'
   },
-  time:{
-    fontSize:25,
-    color:'#F9476C',
-    fontWeight:'600',
+  time: {
+    fontSize: 25,
+    color: '#F9476C',
+    fontWeight: '600',
     fontFamily: 'Mulish-Regular'
+  },
+  container: {
+    backgroundColor: '#00000099',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  view: {
+    width: 350,
+    height: 150,
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  header: {
+    backgroundColor: '#FAA1A1',
+    width: '100%',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  content: {
+    flexDirection: 'row-reverse',
+    flex: 1,
+    margin: 5
+  },
+  ImageStyle: {
+    resizeMode: 'cover',
+    height: 50,
+    width: 50,
   }
 });
 
--- HealthTracking/src/Screens/Setting.js
+++ HealthTracking/src/Screens/Setting.js
@@ -1,30 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Button, View, FlatList,Alert, Text, StyleSheet, TouchableOpacity,Image, Modal } from "react-native";
+import { Button, View, FlatList, Text, StyleSheet, TouchableOpacity, Image, Modal } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import PushNotification from "react-native-push-notification";
 import { ListItem } from 'react-native-elements';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { TextInput } from "react-native-gesture-handler";
-//import { FirebaseManager } from "../Utils/FirebaseManager";
 
 export default function Setting() {
     const [index, setIndex] = useState(1);
-    const [isDateTimePickerVisible,setIsDateTimePickerVisible] = useState(false);
-    const [Modalvisible,setModalVisible] = useState(false);
-    const [modalLabel,setModalLabel] = useState('Alarm');
+    const [isDateTimePickerVisible, setIsDateTimePickerVisible] = useState(false);
+    const [Modalvisible, setModalVisible] = useState(false);
+    const [modalLabel, setModalLabel] = useState('Alarm');
 
     useEffect(() => {
         getData();
-    },[]);
+    }, []);
 
-        const getData = async () =>{
-            AsyncStorage.getItem("storeAlarm").then(data => {
-                if(data !== null) {
-                    setDATA(JSON.parse(data))
-                    console.log('Loaded Alarm')
-                }
-            }).catch((error) => console.log(error));
-        }
+    const getData = async () => {
+        AsyncStorage.getItem("storeAlarm").then(data => {
+            if (data !== null) {
+                setDATA(JSON.parse(data))
+                console.log('Loaded Alarm')
+            }
+        }).catch((error) => console.log(error));
+    }
 
     function GetPushNotification(date) {
         PushNotification.localNotificationSchedule({
@@ -42,46 +41,42 @@ export default function Setting() {
 
     const handleRemoveItem = (id) => {
         setDATA(DATA.filter(item => item.id !== id));
-        
-        AsyncStorage.removeItem("storeAlarm", JSON.stringify(setDATA)).then(() =>{
+
+        AsyncStorage.removeItem("storeAlarm", JSON.stringify(setDATA)).then(() => {
             console.log('Deleted Alarm');
         }).catch(error => console.log(error));
     };
 
     const renderItem = ({ item }) => (
-    <ListItem>           
-        <ListItem.Content>
-        <ListItem.Subtitle style={{fontSize:20,color:'purple',fontWeight:'bold'}}>{item.modalLabel}</ListItem.Subtitle>
-            <ListItem.Title style = {styles.AlarmTittle}>{item.hour}</ListItem.Title>                    
-            <ListItem.Subtitle style={styles.AlarmSubTittle}>{item.day}</ListItem.Subtitle>                 
-        </ListItem.Content>
+        <ListItem>
+            <ListItem.Content>
+                <ListItem.Subtitle style={{ fontSize: 20, color: 'purple', fontWeight: 'bold' }}>{item.modalLabel}</ListItem.Subtitle>
+                <ListItem.Title style={styles.AlarmTittle}>{item.hour}</ListItem.Title>
+                <ListItem.Subtitle style={styles.AlarmSubTittle}>{item.day}</ListItem.Subtitle>
+            </ListItem.Content>
             <Button
-                title ="Remove"
+                title="Remove"
                 color="red"
                 onPress={() => handleRemoveItem(item.id)}
             />
-    </ListItem>     
+        </ListItem>
     );
 
     function AddDay(date) {
-        
-        var newdate={
+
+        var newdate = {
             id: index,
-            modalLabel:modalLabel,
+            modalLabel: modalLabel,
             hour: date.getHours() + ":" + date.getMinutes(),
             day: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(),
         }
-            const newAlarm = [...DATA, newdate]
+        const newAlarm = [...DATA, newdate]
         setIndex(index + 1);
-        
-        AsyncStorage.setItem("storeAlarm", JSON.stringify(newAlarm)).then(() =>{
-            setDATA(newAlarm);
-            console.log('okay');
-        }).catch(error => console.log(error));
-        
-        
-    }
 
+        AsyncStorage.setItem("storeAlarm", JSON.stringify(newAlarm)).then(() => {
+            setDATA(newAlarm);
+        }).catch(error => console.log(error));
+    }
 
     const showDateTimePicker = () => {
         setIsDateTimePickerVisible(true)
@@ -90,90 +85,76 @@ export default function Setting() {
     };
 
     const hideDateTimePicker = () => {
-       setIsDateTimePickerVisible(false)
+        setIsDateTimePickerVisible(false)
     };
 
-    const OnpressOpenModal =() =>{
+    const OnpressOpenModal = () => {
         setModalVisible(true);
     };
 
-    const OnpressCloseModal =()=>{
+    const OnpressCloseModal = () => {
         setModalVisible(false);
     };
 
     const handleConfirm = (date) => {
-        //let Day = new Date(Date.now());
-        //if(Day <= date){
-        AddDay(date);   
+        AddDay(date);
         GetPushNotification(date);
         hideDateTimePicker();
-    //} else {
-        //Alert.alert('Warning','Vui lòng nhập thời gian tương lai. ',[
-           // {text:'OK'}
-        //]);
-        //handleConfirm();
-    //}
     };
 
     return (
         <View style={styles.container}>
-            <Modal 
-            visible= {Modalvisible} 
-            transparent
+            <Modal
+                visible={Modalvisible}
+                transparent
             >
-                
                 <View style={styles.ModalBackground}>
-                <View style={styles.ModalContainer}>
-                    <View style={{marginLeft:'85%',marginTop:'-4%'}}>
-                        <TouchableOpacity onPress = {OnpressCloseModal}>
-                        <Image
-                            source={require('../Image/X.png')}
-                            style={{height:30,width:30}}
+                    <View style={styles.ModalContainer}>
+                        <View style={{ marginLeft: '85%', marginTop: '-4%' }}>
+                            <TouchableOpacity onPress={OnpressCloseModal}>
+                                <Image
+                                    source={require('../Image/X.png')}
+                                    style={{ height: 30, width: 30 }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={{ fontSize: 30, color: 'black' }}>Alarm Label: </Text>
+                        <TextInput
+                            style={{ fontSize: 20 }}
+                            onChangeText={value => setModalLabel(value)}
+                            placeholder="kg"
+                            keyboardType='default'
                         />
-                        </TouchableOpacity>
+                        <View style={{ marginLeft: '85%' }}>
+                            <TouchableOpacity onPress={showDateTimePicker}>
+                                <Image
+                                    source={require('../Image/OK.png')}
+                                    style={{ height: 30, width: 30 }}
+                                />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <Text style ={{fontSize:30,color:'black'}}>Alarm Label: </Text>
-                    <TextInput
-                        style={{fontSize:20}}
-                        onChangeText={value => setModalLabel(value)}
-                        placeholder="kg"
-                        keyboardType='default'
-                    />
-                    <View style={{marginLeft:'85%'}}>
-                        <TouchableOpacity onPress = {showDateTimePicker}>
-                            <Image
-                                source={require('../Image/OK.png')}
-                                style={{height:30,width:30}}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                </View>
                 </View>
             </Modal>
-            
+
             <FlatList
                 data={DATA}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
             />
-            {/* <Button title="Show Date Picker" 
-            onPress={showDateTimePicker} 
-            color='red'
-            /> */}
             <TouchableOpacity
-            onPress={OnpressOpenModal}
+                onPress={OnpressOpenModal}
             >
-            <Image
-                source={require('../Image/AlarmAdd.png')}
-                style={{marginLeft:'80%',marginBottom:'5%'}}
-            />
+                <Image
+                    source={require('../Image/AlarmAdd.png')}
+                    style={{ marginLeft: '80%', marginBottom: '5%' }}
+                />
             </TouchableOpacity>
             <DateTimePickerModal
                 isVisible={isDateTimePickerVisible}
                 mode="datetime"
                 onConfirm={handleConfirm}
                 onCancel={hideDateTimePicker}
-                
             />
         </View>
     );
@@ -182,38 +163,32 @@ export default function Setting() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'white',
-        //justifyContent: 'center',
-        //alignItems: 'center',
+        backgroundColor: 'white',
     },
     ModalBackground: {
-        flex:1,
-        alignContent:'center',
-        justifyContent:'center',
-        //backgroundColor:'grey'
-        
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
     },
     ModalContainer: {
         width: '80%',
-        backgroundColor:'pink',
+        backgroundColor: 'pink',
         paddingHorizontal: 20,
-        paddingVertical: 30 ,
-        borderRadius:20,
-        elevation:20,
-        marginLeft:'10%'
+        paddingVertical: 30,
+        borderRadius: 20,
+        elevation: 20,
+        marginLeft: '10%'
     },
     Alarmcontainer: {
-     backgroundColor: '#FCD0D0', 
-     margin: 10,
-     
+        backgroundColor: '#FCD0D0',
+        margin: 10,
     },
-    AlarmTittle:{
-        fontSize:35,
-        fontWeight:'bold'
+    AlarmTittle: {
+        fontSize: 35,
+        fontWeight: 'bold'
     },
-    AlarmSubTittle:{
-        fontSize:15,
+    AlarmSubTittle: {
+        fontSize: 15,
     },
-    
 });
 
